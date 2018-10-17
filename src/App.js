4ef974bb182bc9fe8e76843c0f8998e680d0f6b9
Nadir Zhumabaev <Nodirbek_zhumabaev@epam.com>
2018-10-17 22:20:07 +0300

    main screen

--- src/App.js
+++ src/App.js
@@ -1,54 +1,23 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
-    );
-  }
-}
-
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.push('Details')}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
-  }
-}
+import { Auth } from './auth-screen';
+import { Main } from './main-screen';
+import { Registration } from './register-screen';
 
 const RootStack = StackNavigator(
   {
-    Home: {
-      screen: HomeScreen,
+    Auth: {
+      screen: Auth,
     },
-    Details: {
-      screen: DetailsScreen,
+    Main: {
+      screen: Main,
+    },
+    Register: {
+      screen: Registration,
     },
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Auth',
   }
 );
 
